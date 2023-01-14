21069b2bec703b0f0017d36abb262a0d45a8e043
shieldsheafson <122336263+shieldsheafson@users.noreply.github.com>
2023-01-14 09:18:27 -0600

    Add files via upload

--- pokersim2.py
+++ pokersim2.py
@@ -1,26 +1,32 @@
 import random as rand
 
-def hand_table(cards,num_hands):
+def hand_table(cards,num_hands): #returns a list of 5 cards, and then a list of cards length num_hands*2 long
     hand_and_table=rand.sample(cards,7+(num_hands-1)*2)
     hands=[]
     for num in range(4,6+(num_hands-1)*2):
         hands.append(hand_and_table.pop())
     return hand_and_table, hands
-def prep_pool(real_pool):
+
+def prep_pool(real_pool): #returns a list of the pool cards as integers, a list of the pool cards as integers without duplicates, and a list of the duplicaters in the pool
     pool=real_pool.copy()
     duplicates=[0,0,0,0,0,0,0,0,0,0,0,0,0]
-    to_remove=[]
-    for num, card in enumerate(pool):
-        pool[num] = int(card[:2])
-        card=int(card[:2])
-        if card==pool[num-1]: to_remove.append(card)
-    for card in to_remove:
-        duplicates[card-2]+=1
-        pool.remove(card)
-    return pool,duplicates
+    straight_pool=[]
+    temp_pool=' '.join(pool).split()
+    pool.clear()
+    for num,card in enumerate(temp_pool):
+        card = int(card[:2])
+        pool.append(card)
+        if num!=0 and pool[num-1]==card: duplicates[card-2]+=1
+        else: straight_pool.append(card)
+    return pool,straight_pool,duplicates
+
+#each hand function returns a score that ranges from 605_040_302 to 81_400_000_000, the first digit represents the hand,
+#and the rest of the digits are the deciding cards in the hand
+ 
 def high_card(pool):
-    return (int(pool[-1][:2])*100_000_000)+(int(pool[-2][:2])*1_000_000)+(int(pool[-3][:2])*10_000)+(int(pool[-4][:2])*100)+(int(pool[-5][:2]))
-def duplicate_hands(pool, duplicates): #pair, three of a kind, four of a kind, 2 pair, full house
+    return pool[-1]*100_000_000+pool[-2]*1_000_000+pool[-3]*10_000+pool[-4]*100+pool[-5]
+
+def duplicate_hands(pool, duplicates): #scores pair, three of a kind, four of a kind, 2 pair, full house
     score=0
     pair=[]
     three=None
@@ -29,27 +35,35 @@ def duplicate_hands(pool, duplicates): #pair, three of a kind, four of a kind, 2
         if item == 1: pair.append(num+2)
         if item == 2: three=num+2
         if item == 3: four=num+2
-    if bool(pair)==True: #blank_mod_pool is a list without the cards that are already in the hand
+    if pair: #blank_mod_pool is a list without the cards that are already in the hand
         pair_mod_pool = [int(x[:2]) for x in pool if pair[-1] != int(x[:2])]
         score=10_000_000_000+(pair[-1]*100_000_000)+(pair_mod_pool[-1]*1_000_000)+(pair_mod_pool[-2]*10_000)+(pair_mod_pool[-3]*100)
     if len(pair)>=2: #Two pair
         two_pair_mod_pool = [int(x[:2]) for x in pool if pair[-1] != int(x[:2]) and pair[-2] != int(x[:2])]
         score=20_000_000_000+(pair[-1]*100_000_000)+(pair[-2]*1_000_000)+(two_pair_mod_pool[-1]*10_000)
-    if three!=None:
+    if three:
         three_mod_pool = [int(x[:2]) for x in pool if three != int(x[:2])]
         score=30_000_000_000+(three*100_000_000)+(three_mod_pool[-1]*1_000_000)+(three_mod_pool[-2]*10_000)
-    if bool(three)==True and bool(pair)==True: score=60_000_000_000+(three*100_000_000)+(pair[-1]*1_000_000) #Full House
-    if four!=None:
+    if three and pair: score=60_000_000_000+(three*100_000_000)+(pair[-1]*1_000_000) #Full House
+    if four:
         four_mod_pool = [int(x[:2]) for x in pool if four != int(x[:2])]
         score=70_000_000_000+(four*100_000_000)+(four_mod_pool[-1]*1_000_000)
-    if score!=0: return score
+    return score
+
 def straight(pool):
+    score=0
     straight=[]
     if len(pool)>=5:
-        if pool[-1]==14 and pool[:4]==[2,3,4,5]: straight=[1,2,3,4,5] #special bit for aces
+        if pool[-1]==14 and pool[:4]==[2,3,4,5]:
+            #straight=[1,2,3,4,5] #special bit for aces
+            score=40_500_000_000
         for num in range(len(pool)-4):
-            if sum(pool[num:num+5])==(pool[num+4]-pool[num]+1)*(pool[num] + pool[num+4])/2: straight=pool[num:num+5]
-    if straight: return 40_000_000_000+straight[-1]*100_000_000
+            if sum(pool[num:num+5])==(pool[num+4]-pool[num]+1)*(pool[num] + pool[num+4])/2:
+                #straight=pool[num:num+5]
+                score=40_000_000_000+pool[num+4]*100_000_000
+    #if straight: return 40_000_000_000+straight[-1]*100_000_000
+    return score
+
 def flush(pool): #and straight flush
     score=0
     score_two=0
@@ -63,13 +77,15 @@ def flush(pool): #and straight flush
             if int(suits[card[-1]][-1][:2])+int(suits[card[-1]][-2][:2])+int(suits[card[-1]][-3][:2])+int(suits[card[-1]][-4][:2])+int(suits[card[-1]][-5][:2])==5*(int(suits[card[-1]][-5][:2])+int(suits[card[-1]][-1][:2]))/2:
                 score_two=80_000_000_000+int(suits[card[-1]][-1][:2])*100_000_000
     if score_two: return score_two
-    elif score: return score
+    else: return score
+
 def scorer(pool): #returns highest score
     pool.sort()
-    prepped_pool, duplicates=prep_pool(pool)
-    scores=[high_card(pool),duplicate_hands(pool,duplicates),flush(pool),straight(prepped_pool)]
-    scores.sort(key=lambda e: (e is not None, e))
+    prepped_pool, straight_pool, duplicates=prep_pool(pool)
+    scores=[high_card(prepped_pool),duplicate_hands(pool,duplicates),flush(pool),straight(straight_pool)] #each hand returns either a number or None
+    scores.sort() #This sorts 
     return scores[-1]
+
 def sim(num_reps, hand): # hand is a string with no spaces: cardnum cardnum o/s
     score=[0,0]
     cards = ['02-s', '03-s', '04-s', '05-s', '06-s', '07-s', '08-s', '09-s', '10-s','11-s', '12-s', '13-s', '14-s', '02-c', '03-c', '04-c', '05-c', '06-c','07-c', '08-c', '09-c', '10-c', '11-c', '12-c', '13-c', '14-c', '02-d','03-d', '04-d', '05-d', '06-d', '07-d', '08-d', '09-d', '10-d', '11-d','12-d', '13-d', '14-d', '02-h', '03-h', '04-h', '05-h', '06-h', '07-h','08-h', '09-h', '10-h', '11-h', '12-h', '13-h', '14-h']
@@ -89,41 +105,9 @@ def sim(num_reps, hand): # hand is a string with no spaces: cardnum cardnum o/s
         elif hand_score<other_hand_score: score[1]+=1
     return(score, 'win % = ' + str((score[0] /num_reps) * 100) + '%', 'win and tie % = ' + str(((score[0] + num_reps-score[0]-score[1]) / num_reps) * 100) + '%')
 
-def sim_two(num_reps, hand): #used for testing, not important
-    score=[0,0,0]
-    cards = ['02-s', '03-s', '04-s', '05-s', '06-s', '07-s', '08-s', '09-s', '10-s','11-s', '12-s', '13-s', '14-s', '02-c', '03-c', '04-c', '05-c', '06-c','07-c', '08-c', '09-c', '10-c', '11-c', '12-c', '13-c', '14-c', '02-d','03-d', '04-d', '05-d', '06-d', '07-d', '08-d', '09-d', '10-d', '11-d','12-d', '13-d', '14-d', '02-h', '03-h', '04-h', '05-h', '06-h', '07-h','08-h', '09-h', '10-h', '11-h', '12-h', '13-h', '14-h']
-    if hand[-1]=='o': hand = [hand[:2] + '-s',hand[2:4] + '-d']
-    else:  hand = [hand[:2] + '-s',hand[2:4] + '-s']
-    cards.remove(hand[0])
-    cards.remove(hand[1])
-    deck=cards.copy()
-    for rep in range(num_reps):
-        current_table = table(cards)
-        current_table_two=current_table.copy()
-        other_hand = hands(1, cards)['hand1']
-        #print(current_table)
-        current_table.extend(hand)
-        hand_score = scorer(current_table)
-        #print(current_table)
-        current_table_two.extend(other_hand)
-        other_hand_score = scorer(current_table_two)
-        #print(current_table_two)
-        #print(str(hand)+' score '+str(hand_score),'\n'+str(other_hand)+' score '+str(other_hand_score))
-        if hand_score>other_hand_score:
-            score[0]+=1
-            print('win')
-        elif hand_score<other_hand_score:
-            score[1]+=1
-            print('lose')
-        else:
-            score[2]+=1
-            print('tie')
-        cards=deck.copy()
-        print('''
-''')
-    return(score, 'win % = ' + str((score[0] /(score[0] + score[1] + score[2])) * 100) + '%', 'win and tie % = ' + str(((score[0] + score[2]) / (score[0] + score[1] + score[2])) * 100) + '%')
-
 ##cards = ['02-s', '03-s', '04-s', '05-s', '06-s', '07-s', '08-s', '09-s', '10-s','11-s', '12-s', '13-s', '14-s', '02-c', '03-c', '04-c', '05-c', '06-c','07-c', '08-c', '09-c', '10-c', '11-c', '12-c', '13-c', '14-c', '02-d','03-d', '04-d', '05-d', '06-d', '07-d', '08-d', '09-d', '10-d', '11-d','12-d', '13-d', '14-d', '02-h', '03-h', '04-h', '05-h', '06-h', '07-h','08-h', '09-h', '10-h', '11-h', '12-h', '13-h', '14-h']
-##print(hand_table(cards,10))
+##pool=['02-s','05-s', '06-s','14-s','02-c', '03-c', '04-c']
+##pool.sort()
+##print(prep_pool(pool))
 
-print(sim(10_000_000,'0503o'))
+sim(10_000_000,'0403o')
